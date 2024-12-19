d362a4a7624c973f3fc3042046b40f7c239c33e1
naknak92 <rafael@MacBook-Pro-de-Rafael.local>
2024-12-19 20:47:28 +0100

    grafana&prom

--- api/main.py
+++ api/main.py
@@ -1,7 +1,6 @@
 import mlflow
 import mlflow.pyfunc
 from flask import Flask, request, jsonify
-from prometheus_client import Counter, generate_latest, REGISTRY
 
 app = Flask(__name__)
 
@@ -12,15 +11,9 @@ mlflow.set_tracking_uri("http://34.170.123.233:5000")  # Remplace avec l'IP de t
 MODEL_URI = "models:/iris_model/1"
 model = mlflow.pyfunc.load_model(MODEL_URI)
 
-# Créer un compteur pour suivre les requêtes
-REQUEST_COUNT = Counter('http_requests_total', 'Total HTTP Requests')
-
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
-        # Incrémenter le compteur à chaque requête
-        REQUEST_COUNT.inc()
-
         # Récupérer les données d'entrée
         data = request.get_json()
 
@@ -37,17 +30,12 @@ def predict():
         prediction = model.predict([features])
         
         # Retourner la prédiction
-        return jsonify({"prediction": prediction.tolist()})
-
+        return jsonify({"prediction": prediction})
+    
     except Exception as e:
         # Afficher l'erreur dans les logs pour aider à déboguer
         print(f"Error: {str(e)}")
         return jsonify({"error": str(e)}), 400
-
-@app.route("/metrics", methods=["GET"])
-def metrics():
-    """Expose les métriques pour Prometheus"""
-    return generate_latest(REGISTRY)
 
 if __name__ == "__main__":
     # Lancer le serveur Flask sur le port 5001
